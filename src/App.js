f5f7f64e7ec8b922c95330049829b082fe967606
jinzza456 <jinzza456@gmail.com>
2022-03-07 18:20:16 +0900

    material ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Customer from './components/customer'
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import Paper from '@material-ui/core/Paper'
+// import { withStyles } from '@material-ui/core/styles'
+//
+// const styles = theme => ({
+//   root: {
+//     width: '100%',
+//     overflowX: "auto"
+//   },
+//   table: {
+//     minWidth : 1080
+//   }
+// })
 
 const customers = [
   {
@@ -31,22 +48,23 @@ const customers = [
 
 function App() {
   return (
-      <div>
-        {
-          customers.map(c => {
-            return (
-                <Customer
-                    key={c.id}
-                    id={c.id}
-                    image={c.image}
-                    name={c.name}
-                    birthday={c.gender}
-                    job={c.job}
-                />
-            );
-          })
-        }
-      </div>
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gneder={c.gender} job={c.job}/>);})}
+          </TableBody>
+        </Table>
+      </Paper>
   );
 }
 
